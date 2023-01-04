78693048b23ccc2552108557fa38a454e0774a8d
Pushpendra <pushpendra.a@moengage.com>
2023-01-04 17:00:47 +0530

    install/update changes

--- app/src/main/java/com/example/ps_news/ApplicationClass.kt
+++ app/src/main/java/com/example/ps_news/ApplicationClass.kt
@@ -1,14 +1,19 @@
 package com.example.ps_news
 
 import android.app.Application
+import android.content.SharedPreferences
 import com.moengage.core.DataCenter
 import com.moengage.core.LogLevel
 import com.moengage.core.MoEngage
+import com.moengage.core.analytics.MoEAnalyticsHelper
 import com.moengage.core.config.LogConfig
+import com.moengage.core.model.AppStatus
 
 class ApplicationClass : Application() {
 
     val APP_ID = ""
+    val NOT_INSTALLED = -1
+    val CURRENT_VERSION = 1
 
     override fun onCreate() {
         super.onCreate()
@@ -21,5 +26,20 @@ class ApplicationClass : Application() {
             .build()
 
         MoEngage.initialiseDefaultInstance(moEngage)
+
+        val pref = getSharedPreferences("APP_INFO", MODE_PRIVATE)
+        val versionNo = pref.getInt("CURRENT_APP_VERSION", -1);
+
+        if(versionNo == NOT_INSTALLED){
+            // Fresh install
+            val edit = pref.edit()
+            edit.putInt("CURRENT_APP_VERSION", 1).apply();
+            MoEAnalyticsHelper.setAppStatus(this, AppStatus.INSTALL)
+        } else if(BuildConfig.VERSION_CODE == CURRENT_VERSION){
+            // Normal login
+        } else {
+            // Update
+            MoEAnalyticsHelper.setAppStatus(this, AppStatus.UPDATE)
+        }
     }
 }
