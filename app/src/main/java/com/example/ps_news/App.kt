1a5c2bb95f75192ede39a93f8a017546fa92e318
Pushpendra <pushpendra.a@moengage.com>
2023-01-04 13:53:56 +0530

    basic covered + encryption trial

--- app/src/main/java/com/example/ps_news/App.kt
+++ app/src/main/java/com/example/ps_news/App.kt
@@ -1,13 +1,17 @@
 package com.example.ps_news
 
+import android.app.Application
 import android.content.Context
 import com.example.ps_news.utils.AppExecutors
 import com.google.gson.Gson
+import com.moengage.core.MoEngage
 
 object App {
     var executors = AppExecutors()
     var gson: Gson = Gson()
     var application: Context? = null
+    lateinit var moEngage: MoEngage
+
 
     fun init(context: Context) {
         application = context
--- app/src/main/java/com/example/ps_news/ApplicationClass.kt
+++ app/src/main/java/com/example/ps_news/ApplicationClass.kt
@@ -1,11 +1,25 @@
 package com.example.ps_news
 
 import android.app.Application
+import com.moengage.core.DataCenter
+import com.moengage.core.LogLevel
+import com.moengage.core.MoEngage
+import com.moengage.core.config.LogConfig
 
 class ApplicationClass : Application() {
+
+    val APP_ID = ""
 
     override fun onCreate() {
         super.onCreate()
         App.init(this)
+
+        val moEngage = MoEngage.Builder(this, APP_ID)
+            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
+//            .enableEncryption()
+            .setDataCenter(DataCenter.DATA_CENTER_1)
+            .build()
+
+        MoEngage.initialiseDefaultInstance(moEngage)
     }
 }
